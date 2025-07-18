a813f2060e8b09169ee6dfd80fb584afec78ad02
Yan Renat <yanrenat@gmail.com>
2025-07-18 05:03:21 +0000

    Tente outra

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,16 +10,30 @@ const Logo = (props: React.SVGProps<SVGSVGElement>) => (
     {...props}
   >
     <path
-      d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z"
+      d="M4 28V22H10"
       stroke="hsl(var(--primary))"
-      strokeWidth="2"
+      strokeWidth={2.5}
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      d="M16 22C19.3137 22 22 19.3137 22 16C22 12.6863 19.3137 10 16 10C12.6863 10 10 12.6863 10 16C10 19.3137 12.6863 22 16 22Z"
+      d="M10 22V16H16"
+      stroke="hsl(var(--primary))"
+      strokeWidth={2.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M16 16V10H22"
       stroke="hsl(var(--accent))"
-      strokeWidth="2"
+      strokeWidth={2.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M22 10V4H28"
+      stroke="hsl(var(--accent))"
+      strokeWidth={2.5}
       strokeLinecap="round"
       strokeLinejoin="round"
     />
