42c3d7696002f664f5464f2c57eb910142efc7c3
Yan Renat <yanrenat@gmail.com>
2025-07-18 05:01:05 +0000

    Nao gostei, tente outra

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -9,31 +9,30 @@ const Logo = (props: React.SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <rect
-      x="4"
-      y="4"
-      width="24"
-      height="24"
-      rx="4"
+    <path
+      d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z"
       stroke="hsl(var(--primary))"
-      strokeWidth={2.5}
+      strokeWidth="2.5"
+      strokeMiterlimit="10"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
-    <circle
-      cx="16"
-      cy="16"
-      r="6"
-      stroke="hsl(var(--primary))"
-      strokeWidth={2.5}
+    <path
+      d="M16 20V12C16 12 17.5 14 20 14"
+      stroke="hsl(var(--accent))"
+      strokeWidth="2.5"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
-      d="M16 16L20.2426 11.7574"
+      d="M16 16C16 16 14.5 18 12 18"
       stroke="hsl(var(--accent))"
-      strokeWidth={2.5}
+      strokeWidth="2.5"
+      strokeMiterlimit="10"
       strokeLinecap="round"
+      strokeLinejoin="round"
     />
-    <circle cx="23" cy="9" r="2" fill="hsl(var(--accent))" />
   </svg>
 );
 
